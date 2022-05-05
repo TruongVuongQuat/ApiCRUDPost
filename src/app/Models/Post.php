ca356c74b8f7882c3be786bbe99ded35e3f2b407
TruongVuongQuat <quattv@vicoders.com>
2022-05-05 16:10:05 +0700

    init

---
+++ src/app/Models/Post.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace VCComponent\Laravel\TestPostManage\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $guarded  = [];
+}
---
+++ src/app/Providers/PostServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace VCComponent\Laravel\TestPostManage\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PostServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+    }
+}
